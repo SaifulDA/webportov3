da9c1ee55b4b34450e9f5ec4c19a3142418f7ec8
SaifulDA <saifuldaulah24@gmail.com>
2025-03-08 23:29:32 +0700

    Style: Add Style Type Writter

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import DownloadButton from "../../components/common/Button";
 import { useTheme } from "../../components/common/ThemeContext"; // Import useTheme
 import profileImage from "/src/assets/images/profile.svg";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   const { theme } = useTheme(); // Destructure theme dari useTheme
@@ -17,9 +18,22 @@ const Hero = () => {
       <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-5xl gap-10">
         {/* Teks Hero */}
         <div className="text-center max-w-lg justify-center items-center">
-          <p className="text-xl italic mb-3">Hello, I&apos;m</p>
-          <h2 className="text-4xl font-bold mb-2 border-b-2 border-gray-800 inline-block">Saiful Daulah Alfarabi</h2>
-          <p className="text-2xl my-6">IT Development</p>
+          <p className="text-xl italic mb-3 font-italianno">Hello, I&apos;m</p>
+          <h2 className="text-4xl font-kreon mb-2 border-b-2 border-gray-800 dark:border-white inline-block">Saiful Daulah Alfarabi</h2>
+           
+           {/* Efek Mengetik */}
+           <div className="text-2xl my-6 font-kreon h-10">
+           
+           <Typewriter
+              words={["IT Development", "Web Developer", "UI/UX Designer", "Cloud Engineer"]}
+              loop={true}
+              cursor
+              cursorStyle="_"
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
+          </div>
 
           {/* Tombol di Tengah */}
           <div className="flex justify-center mt-4">
@@ -29,8 +43,8 @@ const Hero = () => {
 
         {/* Gambar Profile */}
         <div className="relative flex justify-center">
-          <div className="w-48 h-48 md:w-64 md:h-64 rounded-full bg-black overflow-hidden shadow-lg">
-            <img src={profileImage} alt="Profile" className="w-full h-full object-cover" />
+          <div className="w-48 h-48 md:w-64 md:h-64 rounded-full bg-black overflow-hidden shadow-lg ">
+            <img src={profileImage} alt="Profile" className="w-full h-full object-cover transition-transform duration-300 hover:scale-130" />
           </div>
         </div>
       </div>
