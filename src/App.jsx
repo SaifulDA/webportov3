8271aed484cc2de2f440d14a1b780851f5dc6c43
SaifulDA <saifuldaulah24@gmail.com>
2025-03-09 00:43:00 +0700

    Feat: Add Section About and Skills

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Navbar from "./pages/home/Navbar";
 import Hero from "./pages/home/Hero";
 import Features from "./pages/home/Features";
 import Footer from "./pages/home/Footer";
+import About from "./pages/home/About";
+import Skills from "./pages/home/Skills";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <Hero />
+      <About />
+      <Skills />
       <Features />
       <Footer />
     </div>
