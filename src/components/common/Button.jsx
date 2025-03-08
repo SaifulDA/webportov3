2ab06ace1c09bad4e0b112a5099a21b4a6e81590
SaifulDA <saifuldaulah24@gmail.com>
2025-03-08 23:28:30 +0700

    Feat: Add CV Download

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 
 const DownloadButton = () => {
+  const handleDownload = () => {
+    const cvUrl = "../src/assets/cv.pdf"; // Sesuaikan dengan nama file CV
+    window.open(cvUrl, "_blank"); // Membuka file di tab baru
+  };
   return (
     <button
       className="bg-black font-poppins text-white px-4 py-2 rounded-full font-medium hover:bg-blue-600 transition-colors duration-300 flex items-center justify-center dark:bg-white dark:text-black dark:hover:bg-blue-600 dark:hover:text-white transition-transform duration-300 hover:scale-110"
-      onClick={() => console.log('Download CV clicked')}
+      onClick={handleDownload}
     >
       Download CV
     </button>
