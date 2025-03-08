21d2d4d368298925ef9b5b6ae397c095a69bb7d5
SaifulDA <saifuldaulah24@gmail.com>
2025-03-08 22:52:10 +0700

    Style: Add style hover button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const DownloadButton = () => {
   return (
     <button
-      className="bg-black text-white px-4 py-2 rounded-full font-medium hover:bg-gray-600 transition-colors duration-300 flex items-center justify-center dark:bg-white dark:text-black dark:hover:bg-blue-600 dark:hover:text-white"
+      className="bg-black font-poppins text-white px-4 py-2 rounded-full font-medium hover:bg-blue-600 transition-colors duration-300 flex items-center justify-center dark:bg-white dark:text-black dark:hover:bg-blue-600 dark:hover:text-white transition-transform duration-300 hover:scale-110"
       onClick={() => console.log('Download CV clicked')}
     >
       Download CV
