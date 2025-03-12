5383419f41df3992e8f2995c29b442b737a816c3
SaifulDA <saifuldaulah24@gmail.com>
2025-03-12 15:33:14 +0700

    Feat: App Coming Soon page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Project from "./pages/home/Project";
 import NotFound from "./pages/NotFound"; // Import halaman 404
 import Chat from "./pages/contact/Chat";
 import Gallery from "./pages/gallery/Gallery";
+import ComingSoon from "./pages/ComingSoon";
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
         <Route path="*" element={<NotFound />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/comingsoon" element={<ComingSoon />} />
       </Routes>
     </Router>
   );
