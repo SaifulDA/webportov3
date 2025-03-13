dc1c0b021bfc1a058b194072793e0098755f7669
SaifulDA <saifuldaulah24@gmail.com>
2025-03-13 23:52:24 +0700

    Fix: bug error deploy

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Project from "./pages/home/Project";
 import NotFound from "./pages/NotFound"; // Import halaman 404
 import Chat from "./pages/contact/Chat";
 import Gallery from "./pages/gallery/Gallery";
-import ComingSoon from "./pages/comingSoon";
+import CoomingSoon from "./pages/comingSoon";
 import PhotoBooth from "./pages/app/PhotoBooth";
 import CustomizePhoto from "./pages/app/CustomizePhoto";
 import Rembulan from "./pages/gallery/Rembulan";
@@ -36,7 +36,7 @@ const App = () => {
         <Route path="*" element={<NotFound />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/gallery" element={<Gallery />} />
-        <Route path="/comingsoon" element={<ComingSoon />} />
+        <Route path="/comingsoon" element={<CoomingSoon />} />
         <Route path="/photobooth" element={<PhotoBooth />} />
         <Route path="/customize" element={<CustomizePhoto />} />
         <Route path="/rembulan" element={<Rembulan />} />
