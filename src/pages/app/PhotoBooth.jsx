f24aff3a5f1f72f26d70462df08bd49fcd5cfdc5
SaifulDA <saifuldaulah24@gmail.com>
2025-03-21 02:18:34 +0700

    Fix: eslint disable

--- src/pages/app/PhotoBooth.jsx
+++ src/pages/app/PhotoBooth.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../home/Navbar";
